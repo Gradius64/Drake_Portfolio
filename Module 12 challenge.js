cf155cf5c53af5800041d40d87da6d66b6212d17
w3schools--test <66185683+Gradius64@users.noreply.github.com>
2024-11-15 13:18:26 -0600

    initial commit

---
+++ Module 12 challenge.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Your Name</h1>
+            <nav>
+                <ul>
+                    <li><a href="#about">About Me</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#resume">Resume</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
